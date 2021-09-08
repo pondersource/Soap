bace82febde07502efb0fa3d896ced001672fcc6
Triantafullenia-Doumani <Triantafulleniad@gmail.com>
2021-09-08 11:30:06 +0200

    function names for soap server

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # Soap_server
-Soap server in php
 
- # Functions I need for the soap Server
+##  What Soap does
+
+SOAP (formerly an acronym for Simple Object Access Protocol) is a messaging protocol specification for exchanging structured information in the implementation of web services in computer networks
+
+## Soap Client
+
+A SOAP client sends the XML document to a SOAP server. This SOAP request is posted using HTTP or HTTPS to a SOAP Request Handler running as a servlet on a Web server. ... A response from the service is returned to the SOAP Request Handler Servlet and then to the caller using the standard SOAP XML payload format.
+
+## Soap Server
+
+The Web Services Simple Object Access Protocol (SOAP) server is an application server plug-in that receives and sends XML data, and provides XML SOAP interfaces into the Tivoli Management Services components and the monitoring agents.
+
+# Functions I need for the soap Server
 
  1) public __construct()
 
- 2) addSoapHeader()
+ 2) add_header()
 
- 3) addFunction()
+ 3) add_function()
 
- 4) getFunctions()
+ 4) get_functions()
 
- 5) handleSoapRequest()
+ 5) handle_request()
 
- 6) handleSoapFault()
+ 6) handle_fault()
+
+ 7) set_class()
+
+ 8) set_object()
+
+ 9) set_persistence()
+
+
+ # Functions I need for the soap Client
--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 
 // model, which uses in web service functions as parameter
-class server{
+class XmlVars{
   public $amount;
   public $sender;
   public $recipient;
@@ -9,11 +9,58 @@ class server{
   ...
 }
 
-// initialize SOAP Server
-$server=new SoapServer($wsdl,$array);
+# ? initialize SOAP Server
+# return: -
+# parametres:
+  # wsdl: To use the SoapServer in WSDL mode, pass the URI of a WSDL file. Otherwise, pass null and set the uri option to the target namespace for the server.
+  # array: blah blahS
+$server=new Soap_server($wsdl,$array);
 
-$soap->addFunction($function_name);
+# ? Adds a SOAP header to be returned with the response to the current request.
+# return: -
+# parametres:
+  # header: the
+$soap->add_header();
 
-$soap->handle([$soap_request]);
+# return: -
+# parametres: function name as string
+$soap->add_function($function_name);
+
+# return: An array of the defined functions.
+# parametres: -
+$soap->get_functions($);
+
+# return:  -
+# parametres:
+  # request: The SOAP request. If this argument is omitted, the request is assumed to be in the raw POST data of the HTTP request.
+$soap->handle_request([$soap_request]);
+
+$soap->handle_fault();
+
+# returns: -
+# parametres:
+  # class: the name of exported /**
+  # args: These optional parameters will be passed to the default class constructor during object creation.
+  class ClassName extends AnotherClass
+  {
+
+    function __construct(argument)
+    {
+      // code...
+    }
+  }
+
+$soap->set_class();
+
+$server->set_object(new Service());
+
+$soap->set_persistence();
+
+
+# Notes
+# In your class, if you declare a function with the name of the header, the function will be called when that header is received.
+
+
+
 
 ?>
